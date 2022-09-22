ae51b964ab39288ceba594e75263133ba894f0a1
DaHosi69 <113669345+DaHosi69@users.noreply.github.com>
2022-09-22 21:07:47 +0200

    Grundlage
    Grundlage

---
+++ EratosthenesPrimeSieves/src/com/company/EratosthenesPrimeSieve.java
@@ -0,0 +1,16 @@
+package com.company;
+public class EratosthenesPrimeSieve implements PrimeSieves
+{
+
+    public static void main(String[] args)
+    {
+    }
+
+    public boolean[] isPrime()
+    {
+    }
+
+    public void printPrimes()
+    {
+    }
+}
---
+++ EratosthenesPrimeSieves/src/com/company/PrimeSieves.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface PrimeSieves {
+
+
+    public int MAX = 100;
+
+    public boolean[] isPrime();
+
+    public void printPrimes();
+}
