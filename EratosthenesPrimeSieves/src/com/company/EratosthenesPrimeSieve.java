b0e07e6ca65e48a0d79d6897d30f712f7146e589
DaHosi69 <113669345+DaHosi69@users.noreply.github.com>
2022-09-22 21:10:44 +0200

    Add files via upload
    isPrime

--- EratosthenesPrimeSieves/src/com/company/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieves/src/com/company/EratosthenesPrimeSieve.java
@@ -8,7 +8,26 @@ public class EratosthenesPrimeSieve implements PrimeSieves
 
     public boolean[] isPrime()
     {
+
+        boolean[] Primearr = new boolean[MAX+1];
+        for (int i = 2; i <= MAX; i++) {
+            Primearr[i] = true;
+        }
+
+
+        for (int factor = 2; factor*factor <= MAX; factor++)
+        {
+            if (Primearr[factor])
+            {
+                for (int j = factor; factor*j <= MAX; j++)
+                {
+                    Primearr[factor*j] = false;
+                }
+            }
+        }
+        return Primearr;
     }
+
 
     public void printPrimes()
     {
